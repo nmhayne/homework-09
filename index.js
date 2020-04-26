095a935eacd45b3982394dc6850cbbefee834825
noah.hayne@gmail.com <noah.hayne@gmail.com>
2020-04-25 23:22:19 -0400

    not sure how to do this without loops

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // REQUIRED PACKAGES
 const fs = require("fs");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 
 // CONSTANT NAME TO REFER TO README FILE
 const readMeFile = "readme.md";
@@ -28,42 +28,56 @@ const createListItem = item => { return `-${item}` };
 // * Project title
 // PROMPT TO GET PROJECT TITLE
 const getTitle = () => {
-      inquirer.prompt({
-            type: "input",
-            message: "What is your project's title?",
-            name: "title"
-      },{
-            type: "input",
-            message: "Briefly describe the project",
-            name: "description"
-      },{
-            // ASK FOR LICENSE TYPE
-            type: "list",
-            message: "License Type: ",
-            name: "license",
-            choices: ["MIT", "Apache", "GNU", "BSD"]
-      },{
-            type: "input",
-            message: "Briefly describe how to use the project",
-            name: "projectUsage"
-      },{
-            // ASK FOR GITHUB USERNAME
-            type: "input",
-            message: "What is your github username?",
-            name: "username"
-      }     
-      ).then( (answer) => {
-            
+      inquirer
+      .prompt([
+            {
+                  type: "input",
+                  message: "What is your project's title?",
+                  name: "title"
+            },
+            {
+                  type: "input",
+                  message: "Briefly describe the project",
+                  name: "description"
+            },
+            {
+                  // ASK FOR LICENSE TYPE
+                  type: "list",
+                  message: "License Type: ",
+                  name: "license",
+                  choices: ["MIT", "Apache", "GNU", "BSD"]
+            },
+            {
+                  type: "input",
+                  message: "Briefly describe how to use the project",
+                  name: "projectUsage"
+            },
+            {
+                  // ASK FOR GITHUB USERNAME
+                  type: "input",
+                  message: "What is your github username?",
+                  name: "username"
+            }
+      ]).then( (answer) => {
+            readMeString += createTitle(answer.title)+"\n";
+            readMeString += answer.description+"\n";
+            readMeString += answer.license+"\n";
+            readMeString += answer.projectUsage+"\n";
+            readMeString += answer.username+"\n";
+            console.log(readMeString);
       }
       )
 }
 
 
+
 // * Table of Contents
-var tableLength=0;
+let tableLength=0;
+let contentString="";
 // PROMPT TO GET TABLE OF CONTENTS LENGTH
 const getTable = () => {
-      inquirer.prompt({
+      inquirer
+      .prompt({
             type: "number",
             message: "How many items long is the table of contents?",
             name: "contentLength"
@@ -73,9 +87,9 @@ const getTable = () => {
                   inquirer.prompt({
                         type: "input",
                         message: "Enter each content heading?",
-                        name: "packageName"
+                        name: "content"
                   }).then( (answer) => {
-                        requiredPackages += answer.packageName;      
+                        contentString += answer.content+"\n";      
                   })      
             }
       }
@@ -84,6 +98,7 @@ const getTable = () => {
 };
 
 // * Installation
+let requiredPackages="";
 const getInstall = () => {
       inquirer.prompt({
             type: "number",
@@ -97,36 +112,40 @@ const getInstall = () => {
                         message: "Enter the name for each package?",
                         name: "packageName"
                   }).then( (answer) => {
-                        requiredPackages += answer.packageName;      
+                        requiredPackages += answer.packageName+"\n";      
                   })      
             }
       }
       )
 };
 
-
+let contributors="";
 // * Contributing
 const getContributors = () =>{
-      // ASK USER IF TO ADD CONTRIBUTOR
-      var addmore = inquirer.confirm("Would you like to add a contributor?");
-      // WHILE TRUE ADD CONTRIBUTORS
-      while (addmore===true) {
-            inquirer.prompt({
-                  // ASK FOR GITHUB CONTRIBUTOR USERNAME
-                  type: "input",
-                  message: "What is their github username?",
-                  name: "username"
-            }     
-            ).then( (answer) => {
-                  contributors+=answer.username;
-                  addmore = inquirer.confirm("Would you like to add another contributor?");
+      inquirer
+      .prompt({
+            type: "number",
+            message: "How many contributors need to be credited?",
+            name: "contributorsAmount"
+      }).then( (answer) => {
+            console.log(answer.contributorsAmount);
+            let contribs = parseInt(answer.contributorsAmount);
+            for(let i=0; i<contribs,i++;) {
+                  
             }
-            )
-            
       }
+      )
+};
+
+const stupidFunction = () => {
+      inquirer.prompt({
+            type: "input",
+            message: "Enter the GitHub username for each contributor",
+            name: "username"
+      }).then( (answer) => {
+            contributors += answer.username+"\n";      
+      })
 }
 
-
-
-
+getContributors();
 
